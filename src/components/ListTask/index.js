dc9170a2c3a6db2133aa30862c5d8fcc06e258c1
Tiago Moreira <tiago.joke@yahoo.com.br>
2022-02-17 02:54:07 -0300

    refactor: task list

--- src/components/ListTask/index.js
+++ src/components/ListTask/index.js
@@ -1,15 +1,13 @@
 import React from 'react';
 import TaskCard from '../TaskCard'
+import './ListTask.css';
 
-const ListTask = ({listTasks}) => {
-
-  return(
-    <div className="order-card-list">
-      {listTasks.map((task) => (
-        <TaskCard key={ task._id } task={ task } />
-      ))}
-    </div>
-  );
-};
+const ListTask = ({listTasks}) => (
+  <div className="order-card-list">
+    {listTasks.map((task) => (
+      <TaskCard key={ task._id } task={ task } />
+    ))}
+  </div>
+);
 
 export default ListTask;
